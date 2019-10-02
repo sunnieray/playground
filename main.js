7c7f1e90117bf9bd43feb65d31ba1b62a4df7686
sunnieray <sunnieray1@gmail.com>
2019-10-01 20:51:12 -0500

    tuesday

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("test");
+
+var paragraph = document.getElementById("paragraph")
+
+console.log("variable", paragraph);
+
+
+paragraph.addEventListener("click", () => {console.log("clicky"); } )
